b97f6c31c16c75d74bb323a57292961b5ec6983b
despair3 <nospam@>
2024-07-22 21:48:03 +0500

    plot 3 ofz

--- LinearRegression/check_staionary.py
+++ LinearRegression/check_staionary.py
@@ -2,12 +2,10 @@ import load_files as lf
 from merge_open import merge_open
 df = merge_open(lf.df_grouped)
 import matplotlib.pyplot as plt
+import pandas as pd
 
-df.plot(x=0,y=1,kind="scatter")
-plt.show()
+df.index = pd.to_datetime(df.index, format='%Y%m%d')
+# df.iloc[:,0].plot()
+df.plot()
 df.info()
-
-df["y"] = df.iloc[:, 0].shift(-1)
-df= df[-50:]
-df.plot(x=0,y="y", kind="scatter")
 plt.show()
