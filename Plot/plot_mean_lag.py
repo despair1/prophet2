ada5cadc92a05796f6c7d943f993503ef9ddce63
despair3 <nospam@>
2024-07-23 18:22:42 +0500

    strange seaborn regplot

--- Plot/plot_mean_lag.py
+++ Plot/plot_mean_lag.py
@@ -3,6 +3,7 @@ from merge_open import merge_open
 df = merge_open(lf.df_grouped)
 import matplotlib.pyplot as plt
 from val_list2index_list import  val_list2index_list
+import seaborn as sns
 
 df.plot(x=0,y=1,kind="scatter")
 # plt.show()
@@ -11,5 +12,9 @@ df.info()
 df["y"] = df.iloc[:, 0].shift(-1)
 df= df[-50:]
 val_list2index_list(df,0,85,86)
-df.plot(x=0,y="y", kind="scatter")
+fig,ax = plt.subplots()
+ax.plot(df[df.columns[0]], df["y"])
+sns.regplot(x=df[df.columns[0]], y=df["y"] )
 plt.show()
+# df.plot(x=0,y="y", kind="scatter")
+# plt.show()
--- Plot/val_list2index_list.py
+++ Plot/val_list2index_list.py
@@ -5,4 +5,5 @@ def val_list2index_list(df: pd.DataFrame, col_ind, start_val, end_val):
     col_name = df.columns[col_ind]
     filtered_indices = df[
         (df[col_name] >= start_val) & (df[col_name] <= end_val)].index
-    print(filtered_indices)
+    print(filtered_indices, len(filtered_indices))
+    # print(filtered_indices.tolist())
