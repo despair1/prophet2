f6c71a77df0952455dd07eca0157c2bebdda3514
despair3 <nospam@>
2024-07-23 16:59:50 +0500

    val list to index list in scatter plot

--- LinearRegression/plot_hist.py
+++ LinearRegression/plot_hist.py
@@ -8,6 +8,9 @@ import pandas as pd
 # df.iloc[:,0].plot()
 df.plot()
 df.hist()
-plt.tight_layout(rect=[0, 0, 1, 0.95])
+
 # df.info()
+df[-50:].plot()
+df[-50:].hist()
+plt.tight_layout(rect=[0, 0, 1, 0.95])
 plt.show()
--- Plot/plot_mean_lag.py
+++ Plot/plot_mean_lag.py
@@ -2,12 +2,14 @@ import load_files as lf
 from merge_open import merge_open
 df = merge_open(lf.df_grouped)
 import matplotlib.pyplot as plt
+from val_list2index_list import  val_list2index_list
 
 df.plot(x=0,y=1,kind="scatter")
-plt.show()
+# plt.show()
 df.info()
 
 df["y"] = df.iloc[:, 0].shift(-1)
 df= df[-50:]
+val_list2index_list(df,0,85,86)
 df.plot(x=0,y="y", kind="scatter")
 plt.show()
