023372f02adfca3ca9385d1ab51a12dd40d213a2
despair3 <nospam@>
2024-07-24 16:10:02 +0500

    sorted columns by end\start mean day open

--- Screening/plot_day_mean_all.py
+++ Screening/plot_day_mean_all.py
@@ -1,7 +1,12 @@
 from  load_csv_walk import load_csv_walk
 from group_by_day import group_by_day
+from merge_open import merge_open
+import matplotlib.pyplot as plt
+from sort_col_by_risen import sort_col_by_risen
 
 dfs = load_csv_walk()
-
 dfs = group_by_day(dfs)
-print(dfs)
+df = merge_open(dfs)
+df = sort_col_by_risen(df)
+df.plot()
+plt.show()
---
+++ Screening/sort_col_by_risen.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def sort_col_by_risen(df: pd.DataFrame):
+    l = df.iloc[-1]/df.iloc[0]
+    sorted_columns = l.sort_values(ascending=False).index
+    sorted_df = df[sorted_columns]
+    # l = l/ df.loc[0]
+    # print(l, sorted_columns)
+    print(sorted_df)
+    return sorted_df
